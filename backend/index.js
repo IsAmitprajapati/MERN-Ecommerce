be5782be4ffadf5c82341d6de27d3cf4adccdf4c
Amit prajapati <57350935+IsAmitprajapati@users.noreply.github.com>
2023-03-06 21:40:37 +0530

    cors error

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(cors(
-  origin : "https://mern-ecommerce-frontend-nu.vercel.app"
-));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 // const router = express.Router()

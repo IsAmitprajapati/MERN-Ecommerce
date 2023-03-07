ae065f7930e8530b1bd02f2c6e7828b56e13ee99
Amit prajapati <57350935+IsAmitprajapati@users.noreply.github.com>
2023-03-07 09:00:18 +0530

    data accept approx 10 mb

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: false }))
 // const router = express.Router()
 const connectDB = require("./config/db")

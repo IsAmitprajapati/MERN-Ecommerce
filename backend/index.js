b6ef83c3b22ca4ba2e77bf0e661e4b7d20b3292c
Amit prajapati <57350935+IsAmitprajapati@users.noreply.github.com>
2023-03-08 20:33:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(cors());
+app.use(cors(
+  origin : "https://mern-ecommerce-frontend-nu.vercel.app"
+));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: false }))
 // const router = express.Router()

e2a928cf108be51eb1e4103084aadb8a53a72d0a
isamitprajapati <amitprj796@gmail.com>
2023-03-09 11:52:58 +0530

    get product

--- backend/controllers/product/getProduct.js
+++ backend/controllers/product/getProduct.js
@@ -1,6 +1,7 @@
 const ProductModel = require("../../models/product");
 
 module.exports = getProduct = async(req, res) => {
+  console.log("get Product")
   try {
     const data = await ProductModel.find({});
     res.send(data);

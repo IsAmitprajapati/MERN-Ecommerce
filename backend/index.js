4be98e268fd6cf52a08a884329b410f9d8209c0e
isamitprajapati <amitprj796@gmail.com>
2023-03-09 10:46:00 +0530

    change in the connection and server port

--- backend/index.js
+++ backend/index.js
@@ -6,25 +6,28 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-app.use(express.json({limit : "20mb"}));
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: false }));
 // const router = express.Router()
-const connectDB = require("./config/db")
-connectDB()
 
-const routerProduct = require("./routes/productRoute")
-const userRoutes = require("./routes/userRoutes")
-const routerUpload = require("./routes/uploadRoute")
+const routerProduct = require("./routes/productRoute");
+const userRoutes = require("./routes/userRoutes");
+const routerUpload = require("./routes/uploadRoute");
 
-app.use("/product",routerProduct)
-app.use("/user",userRoutes)
-app.use("/upload",routerUpload)
+app.use("/product", routerProduct);
+app.use("/user", userRoutes);
+app.use("/upload", routerUpload);
 
 const PORT = process.env.PORT || 8080;
 
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running" });
+});
 
-
-//Port running 
-app.listen(PORT, () => {
-  console.log(`Server is running ${PORT}`);
+//connected to database then run the server
+const connectDB = require("./config/db");
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running ${PORT}`);
+  });
 });

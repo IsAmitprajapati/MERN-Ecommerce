fefae217be0daeaf8f60f7be625de8bee32e320e
isamitprajapati <amitprj796@gmail.com>
2023-03-16 15:54:28 +0530

    display image

--- frontend/src/page/Cart.js
+++ frontend/src/page/Cart.js
@@ -128,7 +128,7 @@ const Cart = () => {
           </div>
           <div className="lg:hidden flex-1 md:max-w-3xl lg:max-w-md h-10 sticky z-10 bottom-0 left-0 right-0 w-full bg-red-600 flex justify-center items-center my-1 rounded cursor-pointer">
             <p className="font-bold text-base md:text-lg  text-white  md:my-1">
-              Pay <span>₹</span>{user.cartItem.reduce((acc, curr) => acc + curr.total, 0)}
+              Pay <span>₹</span> {user.cartItem.reduce((acc, curr) => acc + curr.total, 0)}
             </p>
           </div>
         </div>
